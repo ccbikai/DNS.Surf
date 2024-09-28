73c725a94e9af92e6f4e1e755df94f35895d6e09
ccbikai <ccbikai@gmail.com>
2024-09-28 10:21:21 +0800

    refactor: replace std-env with environment variable for region selection
    Simplified region configuration by leveraging an environment variable instead of a dependency, enhancing flexibility and reducing external dependencies.

--- config.ts
+++ config.ts
@@ -1,5 +1,3 @@
-import { provider } from 'std-env'
-
 export const SITE = {
   title: 'DNS.Surf',
   tagline: 'Querying DNS Resolution Results in Different Regions Worldwide',
@@ -99,4 +97,4 @@ const CLOUDFLARE_REGIONS = {
   },
 }
 
-export const REGIONS = provider?.includes('cloudflare') ? CLOUDFLARE_REGIONS : VERCEL_REGIONS
+export const REGIONS = process.env.NEXT_PUBLIC_CLOUDFLARE_WORKER_HOST ? CLOUDFLARE_REGIONS : VERCEL_REGIONS
